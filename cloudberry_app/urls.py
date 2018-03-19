130ccb8e6e884dda30b66ab178db8f1c25f177f9
Egil <egil.moller@freecode.no>
2018-03-19 15:54:30 +0100

    Removed old code

--- cloudberry_app/urls.py
+++ cloudberry_app/urls.py
@@ -5,7 +5,6 @@ from . import views
 app_name = 'cloudberry_app'
 
 urlpatterns = [
-    url(r'^netjsonconfig/schema\.json$', views.schemas, name='schema'),
     re_path(r'^cloudberry_app/schema/backend/(?P<schema>.*)$', views.schema_backend, name='backend_schema'),
     re_path(r'^cloudberry_app/schema/dynamic/(?P<schema>.*)$', views.schema_dynamic, name='dynamic_schema'),
     re_path(r'^cloudberry_app/schema/meta$', views.schema_meta, name='meta_schema'),
--- cloudberry_app/views.py
+++ cloudberry_app/views.py
@@ -8,22 +8,6 @@ import os.path
 with open(os.path.join(os.path.dirname(__file__), "json-schema-meta-schema.json")) as f:
     meta_schema = json.load(f)
 
-def schemas(request):
-    authenticated = request.user.is_authenticated
-    if callable(authenticated):
-        authenticated = authenticated()
-    if authenticated:
-        schemas = dict(available_schemas)
-        schemas['meta://'] = meta_schema
-        for backend in cloudberry_app.models.Backend.objects.all():
-            schemas["dynamic://%s" % backend.id] = backend.schema
-        c = json.dumps(schemas)
-        status = 200
-    else:
-        c = login_required_error
-        status = 403
-    return HttpResponse(c, status=status, content_type='application/json')
-
 def check_auth(fn):
     def wrapper(request, *arg, **kw):
         authenticated = request.user.is_authenticated
