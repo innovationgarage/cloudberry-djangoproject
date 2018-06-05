f3f6ac237fb768e5b69509c52254f68c00099805
Egil <egil.moller@freecode.no>
2018-06-05 10:33:09 +0200

    Download os-image support (semi-untested)

--- cloudberry_app/urls.py
+++ cloudberry_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'cloudberry_app'
 
 urlpatterns = [
+    re_path(r'^cloudberry_app/device/image/(?P<device>.*)', views.download_device_image, name='download_device_image'),
     re_path(r'^cloudberry_app/schema/backend/(?P<schema>.*)$', views.schema_backend, name='backend_schema'),
     re_path(r'^cloudberry_app/schema/dynamic/(?P<schema>.*)$', views.schema_dynamic, name='dynamic_schema'),
     re_path(r'^cloudberry_app/schema/meta$', views.schema_meta, name='meta_schema'),
