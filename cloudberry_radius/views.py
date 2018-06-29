9a0e737a9d8a888c953f2a3f0e4daf000e4c9bb3
Egil <egil.moller@freecode.no>
2018-06-29 18:14:13 +0200

    Show total balance

--- cloudberry_radius/views.py
+++ cloudberry_radius/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+import django.db.models
 
 def account_balance(request):
     return render(request,
                   'cloudberry_radius/account_balance.html',
-                  {'accounting': request.user.radius_accounting.all().order_by('-start_time')})
+                  {'balance': request.user.radius_accounting.all().aggregate(django.db.models.Sum('amount'))['amount__sum'],
+                   'accounting': request.user.radius_accounting.all().order_by('-start_time')})
