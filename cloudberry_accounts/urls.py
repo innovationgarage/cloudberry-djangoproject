8c0ee4b4f156a6ec4d6d0344726db3686e40aba6
Egil <egil.moller@freecode.no>
2018-07-01 18:07:32 +0200

    Registration of device owners

--- cloudberry_accounts/urls.py
+++ cloudberry_accounts/urls.py
@@ -9,6 +9,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     url(r'^$', cloudberry_accounts.views.homepage, name='index'),
     url(r'^admin/login/?', RedirectView.as_view(pattern_name='login')),
+    url(r'^accounts/register/$', cloudberry_accounts.views.register, name='registration_register'),
     url(r'^accounts/', include('registration.backends.default.urls')),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='cloudberry_accounts/profile.html'), name='profile'),
     url(r'^login/', auth_views.login, name='login'),
--- cloudberry_accounts/views.py
+++ cloudberry_accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect, resolve_url
 from django.contrib.auth import views as auth_views
+import registration.backends.default.views
+import registration.forms
+import django.forms
+from django.utils.translation import gettext as _
 
 def homepage(request):
     if not request.user.is_authenticated:
@@ -7,3 +11,18 @@ def homepage(request):
     if request.user.is_staff:
         return redirect('admin:index')
     return render(request, 'cloudberry_accounts/index.html')
+
+class CustomRegistrationForm(registration.forms.RegistrationForm):
+    class Meta(registration.forms.RegistrationForm.Meta):
+        fields = registration.forms.RegistrationForm.Meta.fields + ('is_staff',)
+    is_staff = django.forms.ChoiceField(
+         label=_("User type"),
+         choices=[(False, 'Internet user (subscriber)'), (True, 'Hotspot owner')],
+         help_text = "Chose 'Internet user' to be able to surf the internet using one of" +
+         "our hotspot. Chose 'Hotspot owner' if you want to provide internet for" +
+         "your customers or staff."
+    )
+    
+register = registration.backends.default.views.RegistrationView.as_view(
+    form_class=CustomRegistrationForm
+)
